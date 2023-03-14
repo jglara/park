f683057482c03d0c4706bffdd7681051542b41ba
ubuntu <juanmarcosmig@gmail.com>
2023-03-14 16:10:30 +0100

    Fixed error in training function, added NN and parameters update as trining progresses

--- test_mpquic.py
+++ test_mpquic.py
@@ -3,7 +3,7 @@ from park.utils.colorful_print import print_red
 
 import random
 import collections
-
+import time
 import numpy as np
 
 import torch
@@ -59,31 +59,40 @@ class Qnet(nn.Module):
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
-            return random.randint(0,2)
+            return random.randint(0,1)
         else : 
             return out.argmax().item()
             
 def train(q, q_target, memory, optimizer):
-    for i in range(10):
+    for i in range(2):
+        # park.logger.info("Training iteration {} start".format(i))
+        now =  time.time()
         s,a,r,s_prime,done_mask = memory.sample(batch_size)
-
+        # park.logger.info("Training iteration {} sample obtained".format(i))
         q_out = q(s)
+        # park.logger.info("Training iteration {} q".format(i))
         q_a = q_out.gather(1,a)
+        # park.logger.info("Training iteration {} forward done".format(i))
         max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
         target = r + gamma * max_q_prime * done_mask
+        # park.logger.info("Training iteration {} target calculated".format(i))
         loss = F.smooth_l1_loss(q_a, target)
+        # park.logger.info("Training iteration {} loss calculated".format(i))
         
         optimizer.zero_grad()
+        # park.logger.info("Training iteration {} gradient to zero".format(i))
         loss.backward()
+        # park.logger.info("Training iteration {} backpropagation completed".format(i))
         optimizer.step()
+        # park.logger.info("Training iteration {} ended in {}".format(i,time.time()-now))
 
 class RandomAgent(object):
     def __init__(self, state_space, action_space, *args, **kwargs):
         self.state_space = state_space
         self.action_space = action_space
 
-        self.counter = 0
-        self.learning_cycle = 100
+        self.learning_counter = 0
+        self.learning_cycle = 50
         
         self.q = Qnet()
         self.q_target = Qnet()
@@ -94,6 +103,12 @@ class RandomAgent(object):
         self.obs = [0,0,0,0]
         self.epsilon = 0.08
 
+        self.update_counter = 0
+        self.update_cycle = 5
+
+        self. score = 0
+    def update_epsilon(self,epoch,max_epoch):
+        self.epsilon = max(0.01, 0.08 - 0.01*(epoch/(max_epoch/50)))
     def get_action(self, obs, prev_reward, prev_done, prev_info):
         # act = self.action_space.sample()
         #print_red(f"get_action {obs} {prev_reward} {prev_done} {prev_info}")
@@ -104,15 +119,22 @@ class RandomAgent(object):
 
         self.act = self.q.sample_action(torch.from_numpy(np.array(obs)).float(),self.epsilon)
         self.obs = obs
-        #HABLAR CON JOSE, en el ejemplo de github se añade a la coleccion para entrenar la observacion,
-        #la accion que se genera con esa observacion, la recompensa de esa sccion, la observacion que se genera con
-        #esa accion y la done_mask, pero la recompensa y la observacion siguiente no las tenemos hasta el siguiente ciclo.
         
-        self.counter+=1
-        if self.counter == self.learning_cycle:
-            self.counter = 0
+        self.learning_counter+=1
+        self.score += prev_reward
+        if self.learning_counter == self.learning_cycle:
+            self.learning_counter = 0
+            # park.logger.info("Starting training")
+            now = time.time()
             train(self.q, self.q_target, self.memory, self.optimizer)
-        
+            # park.logger.info("Training time {}".format(time.time()-now))
+            self.update_counter +=1
+        if self.update_counter == self.update_cycle:
+            # park.logger.info("a")
+            self.update_counter = 0
+            self.q_target.load_state_dict(self.q.state_dict())
+            park.logger.info("score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
+                                                            self.score/(self.learning_cycle*self.update_cycle), self.memory.size(), self.epsilon*100))
         return self.act
 
 
@@ -121,6 +143,10 @@ def main():
     env = park.make('mpquic')
     agent = RandomAgent(env.observation_space, env.action_space)
     env.run(agent)
-
+    agent.update_epsilon(1,20)
+    env.reset()
+    # for i in range(20):
+    #     agent.update_epsilon(i,20)
+    #     env.reset()
 if __name__ == "__main__":
     main()
